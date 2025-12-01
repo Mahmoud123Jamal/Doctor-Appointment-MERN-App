c65d852bc67005d927882dfa6a988b06b6a4472f
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-01 20:57:19 +0200

    Add User Model and it's types

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
-import app from "./app";
-
 dotenv.config();
+import app from "./app";
+import { connectDb } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
-
+connectDb();
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
