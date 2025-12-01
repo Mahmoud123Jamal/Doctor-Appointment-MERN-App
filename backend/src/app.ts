ddcc1559d06a8f0ea3208ac14e03d8916b4ec6b5
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-01 22:10:53 +0200

    add async await handler Hoc , global error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
+// global error handler
+app.use(errorHandler);
 
 export default app;
--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,1 +1,10 @@
 import { Request, Response } from "express";
+import User from "../models/userModel";
+import { catchAsync } from "../utils/catchAsync";
+const register = catchAsync(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+});
+
+const login = catchAsync(async (req: Request, res: Response) => {});
+
+const getUserInfo = catchAsync(async (req: Request, res: Response) => {});
