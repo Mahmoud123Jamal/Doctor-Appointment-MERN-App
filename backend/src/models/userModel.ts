dcb6da847126c85d2951d9482e7df7ed7abc6dc9
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-01 21:06:17 +0200

    update User model and it's type

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,12 +2,14 @@ import { Schema, model } from "mongoose";
 import { IUser } from "../types/IUsertype";
 
 const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
   email: {
     type: String,
     required: true,
     unique: true,
   },
   password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
 const User = model<IUser>("User", userSchema);
