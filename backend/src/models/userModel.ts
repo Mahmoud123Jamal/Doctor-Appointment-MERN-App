689d35d62c9fa429f7fd96e25d6e002bc81b7d85
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-01 21:00:19 +0200

    update User Model

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../types/IUsertype";
+
+const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: { type: String, required: true },
+});
+
+const User = model<IUser>("User", userSchema);
+export default User;
