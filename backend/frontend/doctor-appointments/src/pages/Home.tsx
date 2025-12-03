0640c0c351e9576de62f940b47153e9fe7236ebb
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-04 01:25:41 +0200

    init frontend and handle configration

---
+++ backend/frontend/doctor-appointments/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Home Page</h1>
+      <p>Welcome to the homepage!</p>
+    </div>
+  );
+};
+
+export default Home;
