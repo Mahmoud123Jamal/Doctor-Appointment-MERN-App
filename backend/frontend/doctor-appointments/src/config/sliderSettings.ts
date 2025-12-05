c5c5c8b1a78b947fc7b1fc536768b01162691efc
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-05 06:38:55 +0200

    sperate Slider settings in config folder

---
+++ backend/frontend/doctor-appointments/src/config/sliderSettings.ts
@@ -0,0 +1,15 @@
+import { type Settings } from "react-slick";
+export const heroSliderSettings: Settings = Object.freeze({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  pauseOnHover: false,
+  arrows: false,
+  centerMode: false,
+  variableWidth: false,
+  cssEase: "linear",
+});
