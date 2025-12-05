9803cd4cd7636ef884d4615e5d5f93426c891343
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-05 06:43:55 +0200

    optimize Home page

--- backend/frontend/doctor-appointments/src/pages/Home.tsx
+++ backend/frontend/doctor-appointments/src/pages/Home.tsx
@@ -1,9 +1,15 @@
-import HeroSlider from "../components/HeroSlider";
 import About from "./About";
+import React, { Suspense } from "react";
+const HeroSlider = React.lazy(() => import("../components/HeroSlider"));
 const Home = () => {
   return (
     <>
-      <HeroSlider />
+      <Suspense
+        fallback={<div className="loading loading-dots loading-xl"></div>}
+      >
+        <HeroSlider />
+      </Suspense>
+
       <About />
     </>
   );
