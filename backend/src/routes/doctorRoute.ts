eb6d63d331467a0c983a224b75675ba2a3ca5fe7
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-06 03:21:03 +0200

    update countDoctors Route

--- backend/src/routes/doctorRoute.ts
+++ backend/src/routes/doctorRoute.ts
@@ -9,7 +9,7 @@ import { upload } from "../middlewares/uploadImageMiddleware";
 const router = Router();
 router.post("/addDoctors", upload.single("image"), addDoctors);
 router.get("/allDoctors", getAllDoctors);
-router.get("/countdoctors", countDoctors);
-router.get("/:id", getDoctorById);
+router.get("/count", countDoctors);
 
+router.get("/:id", getDoctorById);
 export default router;
