d09844c073995143254f9308374257f032be3da1
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-09 19:41:31 +0200

    add ShowAllDoctors page

---
+++ backend/frontend/doctor-appointments/src/pages/ShowAllDoctors.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import type { showDoctor } from "../types/DoctorType";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa6";
+
+function ShowAllDoctors() {
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [doctors, setDoctors] = useState<showDoctor[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        setServerError(null);
+        setLoading(true);
+
+        const res = await api.get("/doctors/allDoctors");
+        const allDoctors = res.data.data?.doctors;
+
+        if (!allDoctors || res.data.status !== "success") {
+          setServerError(res.data.message || "Failed to fetch doctors");
+          return;
+        }
+
+        setDoctors(allDoctors);
+      } catch (err) {
+        console.error("Error fetching doctors:", err);
+        setServerError("Something went wrong while fetching doctors.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
+
+  if (loading) {
+    return (
+      <p className="text-center mt-10 text-blue-600 font-semibold">
+        Loading doctors...
+      </p>
+    );
+  }
+
+  if (serverError) {
+    return (
+      <p className="text-center mt-10 text-red-600 font-semibold">
+        {serverError}
+      </p>
+    );
+  }
+
+  if (doctors.length === 0) {
+    return (
+      <p className="text-center mt-10 text-gray-600 font-medium">
+        No doctors found.
+      </p>
+    );
+  }
+
+  return (
+    <div className="bg-blue-50 mt-5">
+      <h1 className="text-center font-bold text-3xl mb-3 ">Our doctors</h1>
+      <div className="grid grid-cols-1  lg:grid-cols-3 gap-4 p-4">
+        {doctors.map((doctor, index) => (
+          <div
+            key={index}
+            className="card lg:card-side bg-base-100 shadow-sm cursor-pointer rounded-lg "
+          >
+            <figure>
+              <img
+                src={
+                  typeof doctor.image === "string"
+                    ? doctor.image
+                    : "/avatar.jpg"
+                }
+                alt={doctor.name}
+              />
+            </figure>
+            <div className="card-body p-3">
+              <h2 className="card-title text-sm font-bold">{doctor.name}</h2>
+              <p className="text-xs text-gray-600">{doctor.specialization}</p>
+              <p className="text-xs text-gray-500 mt-1">
+                {doctor.description?.slice(0, 100)}...
+              </p>
+              <p className="text-xs font-semibold mt-1">
+                Experience: {doctor.experience} years
+              </p>
+              <div className="card-actions justify-end">
+                <Link to={`/doctor/${doctor._id}`} className="btn btn-primary">
+                  show details
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center my-4">
+        <div className="flex justify-center mt-4">
+          <Link to="/" className="btn btn-primary flex items-center gap-2">
+            <FaArrowLeft />
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShowAllDoctors;
