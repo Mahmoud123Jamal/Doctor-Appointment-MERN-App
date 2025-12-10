ad6a5782acb93c695dcc0760de507ca0280b718d
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-10 03:30:08 +0200

    wrapp app with ToastProvider

--- backend/frontend/doctor-appointments/src/main.tsx
+++ backend/frontend/doctor-appointments/src/main.tsx
@@ -5,12 +5,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ToastProvider } from "./components/ToastProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
-      AuthProvider
+      <ToastProvider position="top-center" autoClose={2000}>
+        <App />
+      </ToastProvider>
     </AuthProvider>
   </StrictMode>
 );
