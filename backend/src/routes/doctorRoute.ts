6750f9aad31f4303ac04bfa6be0ce787d5365fd4
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-10 03:35:28 +0200

    Add relatedSpecializations Route

--- backend/src/routes/doctorRoute.ts
+++ backend/src/routes/doctorRoute.ts
@@ -4,12 +4,14 @@ import {
   getAllDoctors,
   getDoctorById,
   countDoctors,
+  relatedSpecializations,
 } from "../controllers/doctorController";
 import { upload } from "../middlewares/uploadImageMiddleware";
 const router = Router();
 router.post("/addDoctors", upload.single("image"), addDoctors);
 router.get("/allDoctors", getAllDoctors);
 router.get("/count", countDoctors);
-
+router.get("/bySpecialization/:specialization", relatedSpecializations);
 router.get("/:id", getDoctorById);
+
 export default router;
