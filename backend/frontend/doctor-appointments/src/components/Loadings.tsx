9fa1b9e1b7cf1dc0a168dbf7df16ba04629b5546
Mahmoud123Jamal <mahmoud1234goraba@gmail.com>
2025-12-10 03:58:31 +0200

    Add many loading status component

---
+++ backend/frontend/doctor-appointments/src/components/Loadings.tsx
@@ -0,0 +1,38 @@
+export const LoadingSpinner = () => {
+  return (
+    <div>
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+export const LoadingSpinnerPrimary = () => {
+  return (
+    <div>
+      <span className="loading loading-spinner loading-xl text-primary"></span>
+    </div>
+  );
+};
+
+export const LoadingDots = () => {
+  return (
+    <div>
+      <span className="loading loading-dots loading-xl"></span>
+    </div>
+  );
+};
+
+export const LoadingRing = () => {
+  return (
+    <div>
+      <span className="loading loading-ring loading-xl"></span>
+    </div>
+  );
+};
+
+export const LoadingBall = () => {
+  return (
+    <div>
+      <span className="loading loading-ball loading-xl"></span>
+    </div>
+  );
+};
